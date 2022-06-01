96aae64a4ab6c834ac14f8edfcae7733f3cae090
John Wason <jsqzzl@163.com>
2022-06-01 23:23:10 +0800

    1.Abondon using api server,use douyin official api 2.Add new logical progress to analyse video info 3.Manage seprate .cs file to Douyin folder

--- BiliAvatarMAUI/Douyin/DouyinApi.cs
+++ BiliAvatarMAUI/Douyin/DouyinApi.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Dynamic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
@@ -14,9 +15,12 @@ namespace BiliAvatarMAUI.Douyin
         HttpClient _client = new HttpClient();
 
         public HttpClient Client { get => _client; set => _client = value; }
-        public FileSys Fs { get => fs; set => fs = value; }
+        public FileIO Fs { get => fs; set => fs = value; }
+        internal WebIO WebIO { get => webIO; set => webIO = value; }
 
-        FileSys fs = new FileSys();
+        FileIO fs = new FileIO();
+        WebIO webIO = new WebIO();
+        string api_link_video = "https://www.iesdouyin.com/web/api/v2/aweme/iteminfo/?item_ids=";
 
         public async Task<bool> DownloadVideo(string url, string filepath)
         {
@@ -25,9 +29,12 @@ namespace BiliAvatarMAUI.Douyin
             {
                 try
                 {
-                    var resp = await Client.GetByteArrayAsync(url);
-                    //FileIO.WriteBinaryToFile(filepath, resp);
-                    await Fs.WriteBinaryToFile(filepath, resp);
+                    var resps = await WebIO.GetUrl(url);
+                    var orginUrl = resps.StatusCode.ToString().Equals("OK")
+                                    ? (resps.RequestMessage is not null ? resps.RequestMessage.RequestUri : null)
+                                    : null;
+                    var videoBytes = await Client.GetByteArrayAsync(orginUrl);
+                    await Fs.WriteBinaryToFile(filepath, videoBytes);
                     return true;
                 }
                 catch
@@ -42,12 +49,53 @@ namespace BiliAvatarMAUI.Douyin
 
         }
 
-        public async Task<dynamic> GetVideoInfoByApi(string url)
+        public async Task<WebApiV2ByID> GetVideoInfoByApi(string shareCode)
         {
-            var responseStr = await Client.GetStringAsync(url);
-            var converter = new ExpandoObjectConverter();
-            dynamic obj = JsonConvert.DeserializeObject<ExpandoObject>(responseStr, converter);
-            return obj;
+            var url = GetUrlFromShareCode(shareCode);
+            if (string.IsNullOrEmpty(url))
+            {
+                return null;
+            }
+            else
+            {
+                //获取视频ID
+                var videoId = await GetVideoIdFromUrl(url);
+                //构建视频API链接
+                var videoApiUrl = api_link_video + videoId;
+                //获取返回字符串
+                var responseStr = await Client.GetStringAsync(videoApiUrl);
+                var apijson = System.Text.Json.JsonSerializer.Deserialize<WebApiV2ByID>(responseStr);
+                return apijson;
+            }
+        }
+        public string GetUrlFromShareCode(string shareCode)
+        {
+            Regex geturl = new Regex(@"(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})");
+            if (geturl.Match(shareCode).Success)
+            {
+                return geturl.Match(shareCode).Value;
+            }
+            else
+            {
+                return "";
+            }
+        }
+        public async Task<string> GetVideoIdFromUrl(string url)
+        {
+            string videoId = string.Empty;
+            var res = await WebIO.GetUrl(url);
+            //三目用的好，逻辑死得早
+            var orginUrl = res.StatusCode.ToString().Equals("OK")
+                ? (res.RequestMessage is not null ? res.RequestMessage.RequestUri : null)
+                : null;
+            if (orginUrl != null)
+            {
+                var pathUrl = orginUrl.AbsolutePath;
+                var pathparaList = pathUrl.Split('/').ToList();
+                pathparaList = pathparaList.Where(p => !string.IsNullOrEmpty(p)).ToList();
+                videoId = pathparaList.Last();
+            }
+            return videoId;
         }
     }
 }
---
+++ BiliAvatarMAUI/Douyin/FileIO.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiliAvatarMAUI
+{
+    /// <summary>
+    /// Async is important on GUI programs
+    /// </summary>
+    public class FileIO
+    {
+        public async Task<bool> WriteBinaryToFile(string filepath, byte[] bytes)
+        {
+            using (FileStream fs = File.Open(filepath, FileMode.OpenOrCreate))
+            {
+                fs.Seek(0, SeekOrigin.End);
+                await fs.WriteAsync(bytes);
+                return true;
+            }
+        }
+        public async Task<string> TakePath()
+        {
+            if (MediaPicker.Default.IsCaptureSupported)
+            {
+                FileResult photo = await MediaPicker.Default.PickPhotoAsync();
+
+                if (photo != null)
+                {
+                    // save the file into local storage
+                    return photo.FullPath;
+                }
+                else
+                {
+                    return "";
+                }
+            }
+            else
+            {
+                return "";
+            }
+        }        
+    }
+    public static class Verify
+    {
+        public static string FilterillegalCharacters(string filname)
+        {
+            char[] illegalchars = Path.GetInvalidFileNameChars();
+            var filenameChars = filname.ToCharArray();
+            foreach (var illegalchar in illegalchars)
+            {
+                if (filname.Contains(illegalchar))
+                {
+                    filname.Remove(illegalchar);
+                }
+            }
+            return filname;
+        }
+    }
+}
