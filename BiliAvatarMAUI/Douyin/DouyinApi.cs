beeeb2e09a730b27bc1daefd1dac9749297cf587
John Wason <jsqzzl@163.com>
2022-11-23 02:12:31 +0800

    ReConstruct project by other idea,and make it can publish apk that can actually installed in Android device

--- BiliAvatarMAUI/Douyin/DouyinApi.cs
+++ BiliAvatarMAUI/Douyin/DouyinApi.cs
@@ -5,8 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
+//using Newtonsoft.Json;
+//using Newtonsoft.Json.Converters;
 
 namespace BiliAvatarMAUI.Douyin
 {
